2db0b833ed7f42863d7c08f680000852c69ad877
logonrm <logonrm@fiap.com.br>
2017-06-07 23:08:19 -0300

    first commit

---
+++ app/src/main/java/com/example/logonrm/demointent/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.logonrm.demointent;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final int REQUEST_NOVO_USUARIO = 1;
+    @BindView(R.id.etUsername)
+    EditText etUsername;
+    @BindView(R.id.etSenha)
+    EditText etSenha;
+    @BindView(R.id.tvNovoUsuario)
+    TextView tvNovoUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        if(savedInstanceState != null){
+            tvNovoUsuario.setText(savedInstanceState.getString("TEXTO"));
+
+        }
+        requestSmsPermission();
+
+    }
+    @OnClick(R.id.tvNovoUsuario)
+    public void novoUsuarioClick(){
+        Intent novoUsuario = new Intent(this,NovoUsuarioActivity.class);
+        startActivityForResult(novoUsuario, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_NOVO_USUARIO:
+                etUsername.setText(data.getStringExtra("USERNAME"));
+                break;
+        }
+
+
+        }
+    @Override
+    public void  onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("Texto",tvNovoUsuario.getText().toString());
+    }
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
+}
